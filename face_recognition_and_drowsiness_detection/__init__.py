7c004d94d3ea3c8286c362e647439263dd2c84fc
Pradip Solanki <solankipms@gmail.com>
2021-05-03 21:46:36 +0530

    First commit

---
+++ face_recognition_and_drowsiness_detection/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Face Recocognition & Drowsiness Detection."""
+
+__author__ = """Pradip Solanki"""
+__email__ = 'solankipms@gmail.com'
+__version__ = '0.1.0'
---
+++ face_recognition_and_drowsiness_detection/cli.py
@@ -0,0 +1,16 @@
+"""Console script for face_recognition_and_drowsiness_detection."""
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for face_recognition_and_drowsiness_detection."""
+    click.echo("Replace this message by putting your code into "
+               "face_recognition_and_drowsiness_detection.cli.main")
+    click.echo("See click documentation at https://click.palletsprojects.com/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
