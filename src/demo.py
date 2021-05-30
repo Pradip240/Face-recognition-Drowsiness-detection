3dba49450deb1e95f20b32f00df455e28912b07e
Pradip Solanki <73014474+Pradip240@users.noreply.github.com>
2021-05-31 00:16:31 +0530

    added webcam

--- src/demo.py
+++ src/demo.py
@@ -119,7 +119,7 @@ class LCMS:
         thread_sound = threading.Thread(target=beepy.beep, args=(6,), daemon=True) # play alarm sound when running
                                 
         # capture frames from webcam
-        camera = Camera_cv()
+        camera = Camera()
         
         image_holder = st.empty()
 
